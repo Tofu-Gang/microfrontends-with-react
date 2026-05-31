43e0daeaa8054f22eece7e567766fb1d51f6241f
Tofu Gang <tofugangsw@gmail.com>
2026-05-31 15:30:49 +0200

    11. Finishing the Product List

--- products/src/index.js
+++ products/src/index.js
@@ -1,4 +1,5 @@
 import faker from "faker";
 const COUNT = 5;
 let products = Array.from(Array(COUNT), _ => `<div>${faker.commerce.productName()}</div>`).join("");
-console.log(products);
+
+document.querySelector("#dev-products").innerHTML = products;
