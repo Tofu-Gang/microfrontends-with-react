68162f58ab6e9a73a1d523256c58322b447d0556
Tofu Gang <tofugangsw@gmail.com>
2026-06-08 23:06:59 +0200

    17. Scaffolding the Cart

---
+++ cart/webpack.config.js
@@ -0,0 +1,27 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+
+const { ModuleFederationPlugin } = webpack.container;
+
+export default {
+    mode: "development",
+    entry: "./src/index.js",
+    devServer: {
+        port: 8082,
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        }
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "cart",
+            filename: "remoteEntry.js",
+            exposes: {
+                "./CartIndex": "./src/index.js"
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ]
+};
