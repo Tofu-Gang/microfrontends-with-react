cd745881702e2341f57290c1eed6289a56ddd147
Tofu Gang <tofugangsw@gmail.com>
2026-06-08 20:30:40 +0200

    13. Implementing Module Federation

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,13 +1,23 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+
+const { ModuleFederationPlugin } = webpack.container;
 
 export default {
     mode: "development",
+    entry: "./src/index.js",
     devServer: {
         port: 8080
     },
     plugins: [
+        new ModuleFederationPlugin({
+            name: "container",
+            remotes: {
+                products: "products@http://localhost:8081/remoteEntry.js"
+            }
+        }),
         new HtmlWebpackPlugin({
             template: "./public/index.html"
         })
     ]
-}
+};
