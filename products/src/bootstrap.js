6a0f8996ae2e0fc9e4c4c528ac110225bb3c4779
Tofu Gang <tofugangsw@gmail.com>
2026-06-13 16:36:34 +0200

    24. Sub-App Execution Context

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -4,3 +4,17 @@ const COUNT = 5;
 const products = Array.from(Array(COUNT), _ => `<div>${faker.commerce.productName()}</div>`).join("");
 
 document.querySelector("#dev-products").innerHTML = products;
+
+/*
+ * context/situation #1
+ * we are running this file in development isolation
+ * we are using our local index.html file which DEFINITELY has an element with an id of "dev-products"
+ * we want to immediately render our app into that element
+ */
+
+/*
+ * context/situation #2
+ * we are running this file in development or production through the CONTAINER app
+ * NO GUARANTEE that an element with an id of "dev-products" exists
+ * WE DO NOT WANT TO try to immediately render the app
+ */
