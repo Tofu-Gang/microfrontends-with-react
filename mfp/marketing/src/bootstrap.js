3d6762e9af2697e45b75fbf1a64065fbb807e656
Tofu-Gang <45769845+Tofu-Gang@users.noreply.github.com>
2026-07-08 19:52:13 +0200

    Section 4: Linking Multiple Apps Together (#6)
    * Move ecomm project (sections 1 to 3) to a separate directory.
    * 32. Dependency Files
    * 33. Dependency Installation
    * 34. Initial Webpack Config
    * 35. Creating and Merging Development Config
    * 36. Running Marketing in Isolation
    * Fix webpack.common.js regex.
    * 37. Wiring Up React
    * 39. Adding the Pricing and Landing Pages

---
+++ mfp/marketing/src/bootstrap.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Mount function to start up the app
+function mount(element) {
+    ReactDOM.render(
+        <h1>mfp/marketing/src/bootstrap.js: Hi there!</h1>,
+        element
+    );
+}
+
+/*
+ * context/situation #1
+ * we are running this file in development isolation
+ * we are using our local index.html file which DEFINITELY has an element with an id of "_marketing-dev-root"
+ * we want to immediately render our app into that element
+ */
+if(process.env.NODE_ENV === "development") {
+    const element = document.querySelector("#_marketing-dev-root");
+
+    // assuming our container does not have an element with id "_marketing-dev-root"
+    if(element) {
+        // we are probably running in isolation
+        mount(element);
+    }
+}
+
+/*
+ * context/situation #2
+ * we are running this file in development or production through the CONTAINER app
+ * NO GUARANTEE that an element with an id of "_marketing-dev-root" exists
+ * WE DO NOT WANT TO try to immediately render the app
+ */
+export { mount };
